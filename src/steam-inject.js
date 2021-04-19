ae4d847d9447e21d26f3ba45e4d55cde407b3b0f
lingjing <yangchengbin@xinc818.group>
2021-04-19 14:23:03 +0800

    统计充值金额、修复英文统计问题

--- src/steam-inject.js
+++ src/steam-inject.js
@@ -37,7 +37,7 @@ function collectCount($) {
         "手套武器箱钥匙": "https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsUFJ5KBFZv668FFY1naTMdzwTtNrukteIkqT2MO_Uwz5Q6cYhibyXo4rw2ALsrkRoYjuncNCLMlhpEV4XDTk/120x40",
         "棱彩2号武器箱钥匙": "https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsUFJ5KBFZv668FFU1nfbOIj8W7oWzkYLdlPOsMOmIk2kGscAj2erE99Sn2AGw_0M4NW2hIYOLMlhpcmY0CRM/120x40",
         "地平线武器箱钥匙": "https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsUFJ5KBFZv668FFUwnfbOdDgavYXukYTZkqf2ZbrTwmkE6scgj7CY94ml3FXl-ENkMW3wctOLMlhpVHKV9YA/120x40",
-        "反恐精英20周年武器箱": "https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsUFJ5KBFZv668FFU0naHKIj9D7oTgl4LelaGnMuqIwDgFusR337HCpYmhiwzm8ktqMjv2INKLMlhprbp6CTE/120x40",
+        "反恐精英20周年武器箱钥匙": "https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsUFJ5KBFZv668FFU0naHKIj9D7oTgl4LelaGnMuqIwDgFusR337HCpYmhiwzm8ktqMjv2INKLMlhprbp6CTE/120x40",
         "命悬—线武器箱钥匙": "https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsUFJ5KBFZv668FFY5naqQIz4R7Yjix9bZkvKiZrmAzzlTu5AoibiT8d_x21Wy8hY_MWz1doSLMlhpM3FKbNs/120x40",
         "“头号特训”武器箱钥匙": "https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsUFJ5KBFZv668FFUxnaPLJz5H74y1xtTcz6etNumIx29U6Zd3j7yQoYih3lG1-UJqY27xJIeLMlhpaD9Aclo/120x40",
         "幻彩 3 号武器箱钥匙": "https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsUFJ5KBFZv668FFYynaSdJGhE74y0wNWIw_OlNuvXkDpSuZQmi--SrN-h3gey-Uo6YWmlIoCLMlhplhFFvwI/120x40",
@@ -46,21 +46,47 @@ function collectCount($) {
         "光谱武器箱": "https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsUFJ5KBFZv668FFY2nfKadD4U7Y7lwYXexaGlYb3QzjlUvZ0k0ujHptug2VbirkRrNW2md4SLMlhph09hpX0/120x40",
         "伽玛 2 号武器箱钥匙": "https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsVFx5KAVo5PSkKV4xhfGfKTgVvIXlxNPSwaOmMLiGwzgJvJMniO-Zoo_z2wXg-EVvfSmtc78HsNoy/120x40",
         "“野火大行动”武器箱钥匙": "https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsUFJ5KBFZv668FFYxnaeQImRGu4S1x9TawfSmY-iHkmoD7cEl2LiQpIjz3wPl_ERkYWHwLY-LMlhp9pkR_UQ/120x40",
-        "“九头蛇大行动”武器箱": "https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsUFJ5KBFZv668FFY3navMJWgQtNm1ldLZzvOiZr-BlToIsZcoi-yTpdutiVW2-Es4NWjwIo-LMlhpinMS53M/120x40",
-        "反恐精英20周年印花胶囊": "https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsQEl9Jg9SpIW1KgRr7PDbI219792mh5WHkrn1NeLTwTxSu8QmiLvEptumiwW1rRE-MGD1JI_AIAA7ZliCqAS9wOzsm9bi65vhLWPP/120x40"
+        "“九头蛇大行动”武器箱钥匙": "",
+        "反恐精英20周年印花胶囊": "",
+        "“狂牙大行动”武器箱钥匙": "",
+        "反恐精英武器箱钥匙": "",
+        "裂空武器箱钥匙": "",
+        "光谱武器箱钥匙": "",
+        "伽玛武器箱钥匙": ""
     }
     var mapKey = {
         "Danger Zone Case Key": "“头号特训”武器箱钥匙",
+        "Prisma Case Key": "棱彩武器箱钥匙",
         "Prisma 2 Case Key": "棱彩2号武器箱钥匙",
         "Revolver Case Key": "左轮武器箱钥匙",
         "Glove Case Key": "手套武器箱钥匙",
-        "Clutch Case Key": "命悬一线武器箱钥匙",
-        "Fracture Case Key": "裂空武器箱钥匙"
+        "Clutch Case Key": "命悬—线武器箱钥匙",
+        "Fracture Case Key": "裂空武器箱钥匙",
+        "Operation Broken Fang Case Key": "“狂牙大行动”武器箱钥匙",
+        "Operation Hydra Case Key": "“九头蛇大行动”武器箱钥匙",
+        "Operation Wildfire Case Key": "“野火大行动”武器箱钥匙",
+        "Chroma 2 Case Key": "幻彩2号武器箱钥匙",
+        "Chroma 3 Case Key": "幻彩3号武器箱钥匙",
+        "Spectrum Case Key": "光谱2号武器箱钥匙",
+        "Spectrum 2 Case Key": "光谱2号武器箱钥匙",
+        "Horizon Case Key": "地平线武器箱钥匙",
+        "CS:GO Case Key": "反恐精英武器箱钥匙",
+        "CS20 Case Key": "反恐精英20周年武器箱钥匙",
+        "Gamma Case Key": "伽马武器箱钥匙",
+        "Gamma 2 Case Key": "伽玛 2 号武器箱钥匙"
     }
     var result = {}
     var count = 0
+    var amount = 0
+    var currency = '￥'
     $('.wallet_table_row.wallet_table_row_amt_change').each(function () {
-        if ($($(".wht_items div", this)[0]).text() == "Counter-Strike: Global Offensive" && ($(".wht_type div", this)[0] || {}).innerText == "游戏内购买") {
+        // 统计充值卡
+        var expamt = /(已购买|Purchased)\s+(\S)(\d+.\d{2})/.exec(this.innerText.trim())
+        if (expamt != undefined) {
+            currency = expamt[2]
+            amount += parseFloat(expamt[3])
+        }
+        if ($($(".wht_items div", this)[0]).text() == "Counter-Strike: Global Offensive" && ["In-Game Purchase", "游戏内购买"].includes(($(".wht_type div", this)[0] || {}).innerText)) {
             var item = $(".wth_payment", this)[0].innerText.trim()
             if (item.indexOf("钥匙") > 0 || item.indexOf("Case Key") > 0) {
                 var exp = /(\d*)\s*([\s\S]+)/.exec(item)
@@ -69,6 +95,8 @@ function collectCount($) {
                 if (icons[key] == undefined) {
                     if (mapKey[key] != undefined) {
                         key = mapKey[key]
+                    } else {
+                        console.log(key + " 找不到")
                     }
                 }
                 if (result[key] == undefined) {
@@ -96,5 +124,5 @@ function collectCount($) {
         html += "   </td>"
         html += "<td class='item_count'><span class='history_item_name' style='color: #8650AC;font-weight:bold;'>" + result[key] + " ★</span></td></tr>"
     });
-    return { title: "截止 " + new Date().format("yyyy年M月d日") + " 共计开箱 <span style='color: yellow; '>" + count + "</span> 个，预估消费￥" + count * 18.00 + "元", content: html };
+    return { title: "截止 " + new Date().format("yyyy年M月d日") + " 共计开箱 <span style='color: yellow; '>" + count + "</span> 个，充值合计 <span style='color: orange;'>" + currency + "" + + amount + "</span>", content: html };
 }
